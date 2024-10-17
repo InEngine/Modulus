c3f14e2b4b7fb9e21954acbecd68d54594cb1af5
James Johnson <james.johnson@excellentingenuity.com>
2024-10-17 17:52:42 -0400

    Create ModulusServiceProvider.php
    Adding a service provider

---
+++ src/ModulusServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+
+class ModulusServiceProvider extends ServiceProvider
+{
+
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+        
+    }
+}
